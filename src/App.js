a4eb393ebb069389340c79a49223bd3ba1226380
Kyle Rooney <kjrooney@iastate.edu>
2022-12-01 20:30:40 -0600

    Includes Coles Changes (not working yet)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from "react";
 import "./App.css";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import Button from '@mui/material/Button';
 import AppBar from "./AppBar";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { type } from "@testing-library/user-event/dist/type";
-import ViewPhoto from "./SearchCard";
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';import Search from "./SearchResults";
+import Search from "./SearchResults";
 import UploadFormDialog from "./UploadFormDialog";
-
-
-
-const cards = [1, 2, 3, 4, 5];
 
 const theme = createTheme();
 
@@ -39,11 +22,6 @@ function App() {
     setCredentials(response);
   };
 
-  
-
-  
-  
-  
 
 console.log(credentials != null);
 
@@ -90,41 +68,6 @@ console.log(credentials != null);
               <UploadFormDialog credentials={credentials}></UploadFormDialog>
             </Container>
           </Box>
-          <Container sx={{ py: 8 }} maxWidth="md">
-
-
-
-            {/* <Grid container spacing={4}>
-              {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                  <Card
-                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                  >
-                    <CardMedia
-                      component="img"
-                      sx={{
-                        pt: '0%',
-                      }}
-                      image="https://source.unsplash.com/random"
-                      alt="random"
-                    />
-                    <CardContent sx={{ flexGrow: 1 }}>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        Heading
-                      </Typography>
-                      <Typography>
-                        This is a media card. You can use this section to describe the
-                        content.
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid> */}
-
-
-
-          </Container>
         </main>
       </ThemeProvider>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,91 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, modal } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+//import { eventWrapper } from '@testing-library/user-event/dist/utils';
+import UploadFormDialog from './UploadFormDialog';
 
-test('renders learn react link', () => {
+test('Renders our app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Photo Gallery');
   expect(linkElement).toBeInTheDocument();
 });
+
+test('See if upload button exists', async () => {
+  render(<App />);
+  const button = screen.getByText('Upload Photo');
+  expect(button.toBeInTheDocument);
+});
+
+// const Button = ({onClick, children}) => (
+//   <button onClick={onClick}>{children}</button>
+// )
+
+// test('Calls handleClickOpen prop when clicked', () => {
+//   const handleClickOpen = jest.fn()
+//   render(<Button variant="outlined" onClick={handleClickOpen}>Upload Photo</Button>)
+//   fireEvent.click(screen.getByText('Upload Photo'))
+//   expect(handleClickOpen).toHaveBeenCalledTimes(1)
+// });
+
+// test('pop up test', async () => {
+//   render(<App />);
+//   const button = screen.getByRole("button");
+//   userEvent.click(button);
+//   expect(screen.getByText("Select Photo"));
+// });
+
+// test('pop up test', async () => {
+//   render(<App />);
+//   fireEvent.click(screen.getByRole("button", {name:"Upload Photo"}));
+//   expect(screen.getByText("Select Photo"));
+// });
+
+test('pop up test', async () => {
+  render(<UploadFormDialog />);
+  const button = screen.getByRole("button")
+  userEvent.click(button)
+  const element = screen.getByTestId('description')
+    expect(element).toBeInTheDocument()
+});
+
+// test('Click sign in', () => {
+//   render(<App />);
+//   fireEvent.click(screen.getByText('Upload Photo'));
+  
+//   expect(uploadPhoto).toHaveBeenCalledTimes(1);
+// });
+
+
+// test('pop up test', async () => {
+//   render(<App />);
+//   fireEvent.click(screen.getByText('Upload Photo'));
+  
+// });
+// fireEvent.click(screen.getByText('Upload Photo'))
+
+// await waitFor(() =>
+//   // getByRole throws an error if it cannot find an element
+//   screen.getByRole('heading'),
+// )
+
+
+
+
+// test('Tests google sign in popup', () => {
+//   render(<App />);
+//   const { container } = render(<googleauth />)
+//   // container is just a DOM node
+//   const button = container.querySelector('button')
+// });
+
+
+
+//const uploadFormDialogSpy = jest.spyOn(UploadFormDialog, 'UploadFormDialog')
+
+// uploadFormDialogSpy.mockImplementation(()=>{
+//   return(<div data-testid="UploadFormDialog"></div>)
+// })
+
+
+
+
