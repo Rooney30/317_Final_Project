35155fe399c0b9827660f5ce7e17b5f05cd0b48a
Rooney30 <103701369+Rooney30@users.noreply.github.com>
2022-12-12 15:43:54 -0600

    Got rid of unused comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import { render, screen, fireEvent, waitFor, modal } from '@testing-library/react';
 import App from './App';
 import userEvent from '@testing-library/user-event';
-// import { eventWrapper } from '@testing-library/user-event/dist/utils';
 import UploadFormDialog from './UploadFormDialog';
 
 test('Renders our app', () => {
@@ -67,38 +66,3 @@ test('See if google sign in box exists', async () => {
   const signInBox = screen.getByTestId("GoogleSignIn")
   expect(signInBox).toBeInTheDocument();
 });
-
-// test('Click sign in', () => {
-//   render(<App />);
-//   fireEvent.click(screen.getByText('Upload Photo'));
-  
-//   expect(uploadPhoto).toHaveBeenCalledTimes(1);
-// });
-
-
-// test('pop up test', async () => {
-//   render(<App />);
-//   fireEvent.click(screen.getByText('Upload Photo'));
-  
-// });
-// fireEvent.click(screen.getByText('Upload Photo'))
-
-// await waitFor(() =>
-//   // getByRole throws an error if it cannot find an element
-//   screen.getByRole('heading'),
-// )
-
-
-
-
-// test('Tests google sign in popup', () => {
-//   render(<App />);
-//   const { container } = render(<googleauth />)
-//   // container is just a DOM node
-//   const button = container.querySelector('button')
-// });
-
-
-
-
-
